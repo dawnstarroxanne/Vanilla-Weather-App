20a081326f57bd5368b838a268fb4577d843dfc2
dawnstarroxanne <99957190+dawnstarroxanne@users.noreply.github.com>
2022-03-30 14:10:06 +0100

    showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-alert("hello");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "be32b62906045e65c936d999248490f9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
